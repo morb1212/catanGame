c75ef938988392f8f914707cb7fa5c2daa44a589
morb1212 <156122436+morb1212@users.noreply.github.com>
2024-07-17 16:40:47 +0300

    Update settlement.hpp

--- settlement.hpp
+++ settlement.hpp
@@ -20,6 +20,7 @@ namespace ariel {
         int id;
         std::list<int> roads;
         std::list<int> places;
+        std::list<int> settlements;
     public:
     enum type {
         VILLAGE,
